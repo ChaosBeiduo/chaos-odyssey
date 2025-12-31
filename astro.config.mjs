7ba7d80dc18cadc71a054bce7c926e4a93c871c0
Chako <chaosbeiduo@outlook.com>
2025-12-31 10:38:22 +0800

    feat: add expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,30 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import expressiveCode, { astroExpressiveCode } from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
       shikiConfig: {
-          theme: 'material-theme',
+          theme: 'dracula',
       },
   },
 
-  integrations: [mdx()],
+  integrations: [
+      expressiveCode(),
+      astroExpressiveCode({
+          frames: {
+          },
+          styleOverrides: {
+              frames: {
+                  shadowColor: 'white',
+                  editorActiveTabIndicatorHeight: '3px',
+                  editorActiveTabIndicatorTopColor: 'blue',
+                  editorActiveTabForeground: 'gray'
+              },
+          },
+      }),
+      mdx(),
+  ],
 });
